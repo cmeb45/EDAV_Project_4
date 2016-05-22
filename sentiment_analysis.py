0c0966b72fa14dfdd62fbd990d9bf9abde73c901
cmeb45 <cruss713@gmail.com>
2016-05-22 00:36:51 -0400

    Update style

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import pandas as pd
 from vaderSentiment.vaderSentiment import sentiment as vaderSentiment
 from nltk import tokenize
@@ -15,7 +17,8 @@ lines = f.readlines()
 bigline = " ".join(lines)
 stars = bigline.split('***')
 splits = [s.split('\n') for s in stars[1:]]
-tups = [(s[2].strip(), s[3].strip(), s[4].strip(), "".join(s[5:])) for s in splits]
+tups = [(s[2].strip(), s[3].strip(), s[4].strip(), "".join(s[5:]))
+        for s in splits]
 # ("State of the Union Address", President, Date of Speech, Speech)
 speech_df = pd.DataFrame(tups)
 
@@ -33,9 +36,9 @@ sentence_dict_pres["Barack Obama"] = []
 
 
 for i in range(len(speech_df)):
-    if speech_df[1][i] in ["Richard Nixon","Gerald R. Ford",
-    "Jimmy Carter","Ronald Reagan","George H.W. Bush",
-    "William J. Clinton","George W. Bush","Barack Obama"]:
+    if speech_df[1][i] in ["Richard Nixon", "Gerald R. Ford",
+                           "Jimmy Carter", "Ronald Reagan", "George H.W. Bush",
+                           "William J. Clinton", "George W. Bush", "Barack Obama"]:
         lines_list = tokenize.sent_tokenize(speech_df[3][i])
         sentence_dict_pres[speech_df[1][i]].extend(lines_list)
 
@@ -49,9 +52,9 @@ sentiment_dict_pres["William J. Clinton"] = 0.0
 sentiment_dict_pres["George W. Bush"] = 0.0
 sentiment_dict_pres["Barack Obama"] = 0.0
 
-for president in ["Richard Nixon","Gerald R. Ford",
-    "Jimmy Carter","Ronald Reagan","George H.W. Bush",
-    "William J. Clinton","George W. Bush","Barack Obama"]:
+for president in ["Richard Nixon", "Gerald R. Ford",
+                  "Jimmy Carter", "Ronald Reagan", "George H.W. Bush",
+                  "William J. Clinton", "George W. Bush", "Barack Obama"]:
     temp_score = []
     for sentence in sentence_dict_pres[president]:
         vs = vaderSentiment(sentence)
@@ -59,13 +62,12 @@ for president in ["Richard Nixon","Gerald R. Ford",
     sentiment_dict_pres[president] = np.mean(temp_score)
 
 
-
 # AVERAGE SENTIMENT SCORE OF SENTENCES IN EACH SPEECH
 sentence_dict_speech = {}
 for i in range(len(speech_df)):
-    if speech_df[1][i] in ["Richard Nixon","Gerald R. Ford",
-    "Jimmy Carter","Ronald Reagan","George H.W. Bush",
-    "William J. Clinton","George W. Bush","Barack Obama"]:
+    if speech_df[1][i] in ["Richard Nixon", "Gerald R. Ford",
+                           "Jimmy Carter", "Ronald Reagan", "George H.W. Bush",
+                           "William J. Clinton", "George W. Bush", "Barack Obama"]:
         sentence_dict_speech[speech_df[2][i]] = []
 
 
